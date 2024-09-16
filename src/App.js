fb8b341f2c8b807a4d8ff6250afb384b66f0a50e
oceaneEmile <oceane.emile@oclock.school>
2024-09-16 17:42:52 +0200

    init react project,axios and first component : api/home display recent articles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RecentArticles from './components/RecentArticles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bienvenue sur mon blog de voyage</h1>
+      <RecentArticles /> 
     </div>
   );
 }
